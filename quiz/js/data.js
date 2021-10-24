dbd63b887f12808dbba1100a1584f3f8793653dd
Rakesh Gondala <gondalarakesh2000@gmail.com>
2021-10-24 14:50:35 +0530

    Added Quiz Project

---
+++ quiz/js/data.js
@@ -0,0 +1,42 @@
+var jsonData = [
+    {
+        "q" : "1) The common element which describe the web page, is ?",
+        "opt1" : "heading",
+        "opt2" : "paragraph",
+        "opt3" : "All of these",
+        "opt4" : "None of these",
+        "answer" : "All of these"
+    },
+    {
+        "q" : "2) HTML stands for?",
+        "opt1" : "Hyper Text Markup Language",
+        "opt2" : "High Text Markup Language",
+        "opt3" : "Hyper Tabular Markup Language",
+        "opt4" : "None of these",
+        "answer" : "Hyper Text Markup Language"
+    },
+    {
+        "q" : "3) which of the following tag is used to mark a begining of paragraph ?",
+        "opt1" : "TD",
+        "opt2" : "br",
+        "opt3" : "P",
+        "opt4" : "None of these",
+        "answer" : "P"
+    },
+    {
+        "q" : "4)From which tag descriptive list starts ?",
+        "opt1" : "LL",
+        "opt2" : "DL",
+        "opt3" : "DD",
+        "opt4" : "None of these",
+        "answer" : "DL"
+    },
+    {
+        "q" : "5) Correct HTML tag for the largest heading is _____",
+        "opt1" : "h1",
+        "opt2" : "h6",
+        "opt3" : "heading",
+        "opt4" : "None of these",
+        "answer" : "h1"
+    }
+];
